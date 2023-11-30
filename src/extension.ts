90b7aff145f25f1f29e4dc35932b65dee6dc9e30
Sebastian Warnholz <wahani@gmail.com>
2023-11-30 14:39:19 +0100

    Remove smart execute and switch to inwtlab publisher

--- src/extension.ts
+++ src/extension.ts
@@ -6,161 +6,25 @@ import * as vscode from "vscode";
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     updateWorkspaceSettingsAccordingToINWTStandards();
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand("inwt-python-ide.smartExecAndStep", async () => {
-            let engine = getConfigEngine();
-            let editor = vscode.window.activeTextEditor;
-            if (editor) {
-                let selection = smartSelect();
-                await delay();
-                if (engine === "python") {
-                    selectionToRepl(selection);
-                } else if (engine === "jupyter") {
-                    vscode.commands.executeCommand("jupyter.execSelectionInteractive", selection);
-                }
-                step(editor);
-            }
-        }),
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand("inwt-python-ide.debugExecAndStep", async () => {
-            let editor = vscode.window.activeTextEditor;
-            if (editor) {
-                smartSelect();
-                await delay();
-                vscode.commands.executeCommand("editor.debug.action.selectionToRepl");
-                step(editor);
-            }
-        }),
-    );
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-async function selectionToRepl(selection: string) {
-    selection = sanitizeSelection(selection);
-    await vscode.commands.executeCommand("python.startREPL");
-    if (vscode.window) {
-        let terminal = vscode.window.activeTerminal;
-        terminal?.sendText(selection, true);
-    }
-}
-
-function sanitizeSelection(code: string) {
-    let rootIndentation = levelOfIndentation(code);
-    let codeLines = code.split("\n");
-    // trim indentation:
-    let rootIndentationRegExp = RegExp(
-        "^\\s{indentation-level}".replace("indentation-level", rootIndentation.toString()),
-    );
-    codeLines = codeLines.map((line) => {
-        return line.replace(rootIndentationRegExp, "");
-    });
-    // fill empty lines with correct indentation
-    for (let i = 0; i < codeLines.length; i++) {
-        if (codeLines[i] === "") {
-            let j = 1;
-            while (i + j < codeLines.length) {
-                // the next non empty line will decide the indentation level
-                if (/^.+$/.test(codeLines[i + j])) {
-                    codeLines[i] = indentationAsString(codeLines[i + j]);
-                    break;
-                }
-                j++;
-            }
-        }
-    }
-    // glue back together
-    code = codeLines.join("\n");
-    // properly exit code block
-    if (levelOfIndentation(codeLines[codeLines.length - 1]) > 0) {
-        code = code.concat("\n\n");
-    }
-    return code;
-}
-
-function step(editor: vscode.TextEditor) {
-    if (editor && getConfigStep()) {
-        let line = editor.document.lineAt(editor.selection.active.line);
-        while (isNotLastLine(line.lineNumber, editor.document.lineCount)) {
-            line = editor.document.lineAt(line.lineNumber + 1);
-            if (lineIsCode(line) && !isDecorator(line.text)) {
-                break;
-            }
-        }
-        editor.selection = newSelectionForCursor(line);
-        editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.start));
-    }
-}
-
-function smartSelect() {
-    let editor = vscode.window.activeTextEditor;
-    let selection = "";
-    if (editor) {
-        if (editor.selection.isEmpty) {
-            // We respect the selection of the user and leave it to
-            // juypyter extension to handle all edge cases.
-            // When the selection is empty, we expand the selection to the
-            // next 'best' thing:
-            //   1. When we are in the defining line of a block (class,
-            //      function, loop, control structure): select the entire
-            //      block
-            //   2. When we are not in case 1 or 2, select the current line
-            let line = editor.document.lineAt(editor.selection.active.line);
-            if (getConfigSmartExecute()) {
-                // select the code block
-                let endLine = findEndLineOfPythonCodeBlock(line, editor.document);
-                let startLine = findStartLineOfPythonCodeBlock(line, editor.document);
-                editor.selection = newSelectionForBlock(startLine, endLine);
-            } else {
-                // otherwise use current line
-                editor.selection = newSelectionForLine(line);
-            }
-        }
-        selection = editor.document.getText(
-            new vscode.Range(editor.selection.start, editor.selection.end),
-        );
-    }
-    return selection;
-}
-
 function updateWorkspaceSettingsAccordingToINWTStandards() {
     if (getConfigUpdateSettings()) {
+        setWorkspaceSetting("files", "trimTrailingWhitespace", true);
+        setWorkspaceSetting("files", "insertFinalNewline", true);
         setWorkspaceSetting("python", "autoComplete.addBrackets", true);
         setWorkspaceSetting("python", "linting.flake8Enabled", true);
         setWorkspaceSetting("python", "terminal.activateEnvInCurrentTerminal", true);
         setWorkspaceSetting("python", "testing.pytestEnabled", true);
         setWorkspaceSetting("python", "formatting.provider", "black");
-        setWorkspaceSetting("files", "autoSave", "afterDelay");
         setWorkspaceSetting("python", "formatting.blackPath", "black");
         setWorkspaceSetting("python", "sortImports.path", "isort");
         setWorkspaceSetting("python", "languageServer", "Pylance");
-        setWorkspaceSetting("python", "linting.mypyEnabled", true);
-        setWorkspaceSetting("rewrap", "wrappingColumn", 80);
-        setWorkspaceSetting("git", "autofetch", true);
-        setWorkspaceSetting("diffEditor", "renderSideBySide", true);
-        setWorkspaceSetting("diffEditor", "ignoreTrimWhitespace", true);
-        setWorkspaceSetting("gitlens", "currentLine.enabled", false);
-        setWorkspaceSetting("gitlens", "hovers.enabled", false);
-        setWorkspaceSetting("gitlens", "hovers.currentLine.over", "line");
-        setWorkspaceSetting("gitlens", "codeLens.enabled", false);
-        setWorkspaceSetting("gitlens", "defaultDateStyle", "absolute");
-        setWorkspaceSetting("tabnine", "experimentalAutoImports", true);
-        setWorkspaceSetting("cSpell", "enabled", false);
-        setWorkspaceSetting("cSpell", "language", "de,de-DE,en");
-        setWorkspaceSetting("files", "trimTrailingWhitespace", true);
-        setWorkspaceSetting("files", "insertFinalNewline", true);
         setWorkspaceSetting("python", "analysis.completeFunctionParens", true);
         setWorkspaceSetting("vsintellicode", "sql.completionsEnabled", false);
-        setWorkspaceSetting(
-            "githubIssues",
-            "issueBranchTitle",
-            "feature/${issueNumber}_${sanitizedIssueTitle}",
-        );
-        setWorkspaceSetting("errorLens", "messageEnabled", false);
     }
 }
 
@@ -176,146 +40,6 @@ async function setWorkspaceSetting(
     }
 }
 
-function getConfigSmartExecute(): boolean {
-    return vscode.workspace.getConfiguration("INWT.Python.IDE").smartExecute.blockSelect;
-}
-
 function getConfigUpdateSettings(): boolean {
-    return vscode.workspace.getConfiguration("INWT.Python.IDE").updateWorkspaceSettings;
-}
-
-function getConfigEngine(): string {
-    return vscode.workspace.getConfiguration("INWT.Python.IDE").smartExecute.engine;
-}
-
-function getConfigStep(): boolean {
-    return vscode.workspace.getConfiguration("INWT.Python.IDE").smartExecute.step;
-}
-
-function getConfigDelay(): number {
-    return vscode.workspace.getConfiguration("INWT.Python.IDE").smartExecute.delay;
-}
-
-function findStartLineOfPythonCodeBlock(line: vscode.TextLine, document: vscode.TextDocument) {
-    // we check if there are any decorators before the current line. If so,
-    // we find the most top level and return that as starting line
-    let finalLine = line;
-    while (line.lineNumber - 1 >= 0) {
-        line = document.lineAt(line.lineNumber - 1);
-        if (isDecorator(line.text)) {
-            // is it a decorator?
-            finalLine = line;
-        } else {
-            // if not we just stop here
-            break;
-        }
-    }
-    return finalLine;
-}
-
-function findEndLineOfPythonCodeBlock(line: vscode.TextLine, document: vscode.TextDocument) {
-    let rootIndentation = levelOfIndentation(line.text);
-    let finalLine = line;
-    while (isNotLastLine(line.lineNumber + 1, document.lineCount)) {
-        line = document.lineAt(line.lineNumber + 1);
-        if (lineIsCode(line)) {
-            if (
-                levelOfIndentation(line.text) <= rootIndentation &&
-                !isExcept(line.text) &&
-                !isFinally(line.text) &&
-                !isElif(line.text) &&
-                !isElse(line.text) &&
-                !isClosingParanthesis(line.text)
-            ) {
-                break;
-            } else {
-                finalLine = line;
-            }
-        }
-    }
-    return finalLine;
-}
-
-function isClosingParanthesis(text: string) {
-    return /^\s*[\)\]\}]+/.test(text);
-}
-
-function isDecorator(text: string) {
-    return /^\s*@/.test(text);
-}
-
-function isExcept(text: string) {
-    return /^\s*except[\s:]/.test(text);
-}
-
-function isFinally(text: string) {
-    return /^\s*finally:/.test(text);
-}
-
-function isElif(text: string) {
-    return /^\s*elif\s/.test(text);
-}
-
-function isElse(text: string) {
-    return /^\s*else:/.test(text);
-}
-
-function levelOfIndentation(line: string) {
-    let regexpMatch = line.match(/^\s*/);
-    if (regexpMatch === null) {
-        return 0;
-    } else {
-        return regexpMatch[0].length;
-    }
-}
-
-function indentationAsString(line: string) {
-    let regexpMatch = line.match(/^\s*/);
-    if (regexpMatch === null) {
-        return "";
-    } else {
-        return regexpMatch[0];
-    }
-}
-
-function newSelectionForBlock(line: vscode.TextLine, endLine: vscode.TextLine) {
-    return new vscode.Selection(
-        line.lineNumber,
-        line.range.start.character,
-        endLine.lineNumber,
-        endLine.range.end.character,
-    );
-}
-
-function newSelectionForCursor(line: vscode.TextLine) {
-    return new vscode.Selection(
-        line.lineNumber,
-        line.range.start.character,
-        line.lineNumber,
-        line.range.start.character,
-    );
-}
-
-function newSelectionForLine(line: vscode.TextLine) {
-    return new vscode.Selection(
-        line.lineNumber,
-        line.range.start.character,
-        line.lineNumber,
-        line.range.end.character,
-    );
-}
-
-function isNotLastLine(currentLine: number, lineCount: number) {
-    return currentLine < lineCount - 1;
-}
-
-function lineIsCode(line: vscode.TextLine | undefined) {
-    // A line is empty when its empty, contains only whitespaces or a comment
-    let lineText = line?.text || "";
-    return !(line?.isEmptyOrWhitespace || /^\s*#/.test(lineText));
-}
-
-function delay() {
-    let ms: number = getConfigDelay();
-    return new Promise((resolve) => setTimeout(resolve, ms));
+    return vscode.workspace.getConfiguration("INWT.Python.IDE.V2").updateWorkspaceSettings;
 }
