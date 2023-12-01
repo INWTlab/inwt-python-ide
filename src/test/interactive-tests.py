f2aeab83baac6ce894e17834e6423cae0d1490f6
Sebastian Warnholz <wahani@gmail.com>
2023-12-01 22:37:02 +0100

    Update configuration

--- src/test/interactive-tests.py
+++ src/test/interactive-tests.py
@@ -1,7 +1,6 @@
 
 from datetime import date as very_long_name_which_needs_a_linebreak_after_at_least_100_chars_t
 
-
 import datetime
 from typing import Optional, List
 
@@ -9,3 +8,8 @@ def this_is_a_long_line(with_a_lot_of_symbols: int, to_make_sure_black_is_actual
     # datetime.date()
     # very_long_name_which_needs_a_linebreak_after_at_least_100_chars_as_long_as_you_are_still_sane
     return with_a_lot_of_symbols + to_make_sure_black_is_actually_running_and_breaks_lines_at_100_chars
+
+
+import pandas as pd
+
+pd.DataFrame([1, 2, 3]).isnull()
