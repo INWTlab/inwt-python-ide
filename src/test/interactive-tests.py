8c7bcfa8a578050fa2b49316eb80c6d082e91ab6
Sebastian Warnholz <wahani@gmail.com>
2023-12-02 16:57:52 +0100

    Update and reduce configuration

--- src/test/interactive-tests.py
+++ src/test/interactive-tests.py
@@ -13,3 +13,17 @@ def this_is_a_long_line(with_a_lot_of_symbols: int, to_make_sure_black_is_actual
 import pandas as pd
 
 df: list[int] = pd.DataFrame([1, 2, 3]).isnull()
+
+def my_complect_function():
+    my_list = []
+    for i in range(100):
+        if i > 1:
+            my_list.append(i)
+            if i < 20:
+                print("%d" % "asd")
+                pass
+
+    return my_list
+
+pwd = "askjahsd 918231 3hf0913 kjlkjf sdf,.-"
+secret = "secret_user"
